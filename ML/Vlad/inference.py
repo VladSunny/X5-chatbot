b4e099e710b5798094095d59679fe5ddfb54ea37
VladG00dman <68503186+VladSunny@users.noreply.github.com>
2025-05-03 15:12:50 +0300

    inference

---
+++ ML/Vlad/inference.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from transformers import AutoTokenizer, AutoModel
+import torch
+import os
+import numpy as np
+import faiss
+from sentence_transformers import SentenceTransformer
+from typing import List, Tuple
+import json
+
+index = faiss.read_index("my_index.faiss")
+
+model = SentenceTransformer("Tochka-AI/ruRoPEBert-e5-base-2k")
+
+new_question = "Хочу в отпуск"
+
+with open("data.json", "r", encoding="utf-8") as f:
+    q_a = json.load(f)
+
+questions = list(q_a.keys())
+
+def get_answer(text):
+    query_embedding = model.encode([text])
+    query_embedding = query_embedding.astype('float32')
+    faiss.normalize_L2(query_embedding.reshape(1, -1))
+
+    D, I = index.search(query_embedding, k=1)
+
+    return q_a[questions[I[0][0]]]
+
+# Поиск
+# query_embedding = model.encode([new_question])
+# query_embedding = query_embedding.astype('float32')
+# faiss.normalize_L2(query_embedding.reshape(1, -1))
+
+# D, I = index.search(query_embedding, k=1)  # Топ-1 результат
+# print(f"Ближайший вопрос: {questions[I[0][0]]} (сходство: {D[0][0]:.2f})")
+# print(f"Ответ: {q_a[questions[I[0][0]]]}")
+
+# print(get_answer("Хочу какать"))
